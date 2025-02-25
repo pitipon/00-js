07ecd43287df27a059e7c31cae83ca0e9d3c2000
mo <pitipon@gmail.com>
2025-02-25 15:52:38 +0800

    save

---
+++ 00-for2/for.js
@@ -0,0 +1,12 @@
+function printStarReverse(n) {
+    for (let i = n; i >= 1; i--) {
+        let stars = "";
+        for (let j = 1; j <= i; j++) {
+            stars += "*";
+        }
+        console.log(stars);
+    }
+}
+
+// Example usage:
+printStarReverse(4);
---
+++ 00-for2/test.js
@@ -0,0 +1,13 @@
+function printStar(n) {
+    // TODO: here
+}
+
+// Example usage:
+printStar(4);
+
+
+// RESULT
+// *
+// **
+// ***
+// ****
